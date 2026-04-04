17ba850565b82c931f85d6c542f03bfca284a7de
Fernando Quintero <ferdanielq@outlook.com>
2026-04-04 20:12:15 +0000

    feat: add bd (beads) to dev shell

--- flake.nix
+++ flake.nix
@@ -10,10 +10,29 @@
     let
       system = "x86_64-linux";
       pkgs   = nixpkgs.legacyPackages.${system};
+
+      bd = pkgs.stdenv.mkDerivation {
+        pname   = "beads";
+        version = "1.0.0";
+        src = pkgs.fetchurl {
+          url    = "https://github.com/gastownhall/beads/releases/download/v1.0.0/beads_1.0.0_linux_amd64.tar.gz";
+          sha256 = "7057db1e92428fcf5c08d5dc6b07ead57e588b262cba78b9a26893d55bd29fdb";
+        };
+        sourceRoot   = ".";
+        nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+        buildInputs       = [ pkgs.stdenv.cc.cc.lib pkgs.icu74 ];
+        installPhase = ''
+          mkdir -p $out/bin
+          cp bd $out/bin/bd
+        '';
+      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        packages = [ pkgs.bun ];
+        packages = [
+          pkgs.bun
+          bd
+        ];
       };
     };
 }
